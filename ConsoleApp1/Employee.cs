f6e69820685a2196d1caa4f445a33130383c59f6
Ilias Tsoukalas <i.tsoukalas@outlook.com>
2022-02-13 20:24:09 +0200

    Initial

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee:Person
+    {
+        public string Department{ get; set; }
+        public Employee()
+        {
+            Person person = new Person();
+            person.Name = "hlias tsoukalas";
+        }
+    }
+}
